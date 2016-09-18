2ec2114403da64c2ac7df288ccbe4bf425d8781f
William Goodall <wgoodall01@gmail.com>
2016-09-18 17:30:16 -0400

    Connect to Heroku Postgres

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 "use strict";
-let express      = require("express");
-let compression  = require("compression");
-let morgan = require("morgan");
+let assert      = require("assert");
+let compression = require("compression");
+let express     = require("express");
+let morgan      = require("morgan");
+let pg          = require("pg");
+let pgcs        = require("pg-connection-string");
+let util        = require("util");
 
 let app = express();
+
+// Make sure vital env vars exist
+[
+    "NODE_ENV",
+    "PORT",
+    "DATABASE_URL"
+]
+.forEach(function(name){
+    assert.ok(process.env[name], name + " is not set in env.");
+});
 
 // Middleware
 app.use(compression());
@@ -16,7 +30,16 @@ app.use(morgan(
 // Serve static resources
 app.use(express.static(__dirname + "/static"));
 
+// Connect to postgres
+let urlConfig = pgcs.parse(process.env.DATABASE_URL);
+let dbConfig = util._extend(urlConfig, {
+    max: 20,
+    idleTimeoutMillis: 10000
+});
+
+global.db = new pg.Pool(dbConfig);
+
 
 // Listen on PORT
 console.log("Listening on http://localhost:8080");
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT);
