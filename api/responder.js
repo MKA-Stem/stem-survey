db84ca45dad3fb6b05e2c24c59a606f56d3055a5
William Goodall <wgoodall01@gmail.com>
2016-09-19 16:24:36 -0400

    Implement response API and error handler

---
+++ api/responder.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let assert = require("assert");
+
+module.exports = function(db){
+    return function(req, res, next){
+
+        [
+            req.body.choice,
+            req.body.firstname,
+            req.body.lastname,
+            req.body.yog,
+        ]
+        .forEach(val => assert.ok(val));
+
+
+        db.query(
+            "INSERT INTO responses(choice, firstname, lastname, yog, email) VALUES ($1, $2, $3, $4, $5)",
+            [req.body.choice, req.body.firstname, req.body.lastname, req.body.yog, req.body.email],
+            function(err){
+                // Handle constraint violations
+                if(err && err.constraint){ return next(400); }
+                else if (err) {return next(500); }
+                
+                // Respond nicely
+                res.status(200).json({
+                    status:"ok"
+                });
+            }
+        );
+
+    };
+};
--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 "use strict";
 let assert      = require("assert");
+let bodyParser  = require("body-parser");
 let compression = require("compression");
 let express     = require("express");
+let httpStatus  = require("http-status-codes");
 let morgan      = require("morgan");
 let pg          = require("pg");
 let pgcs        = require("pg-connection-string");
 let util        = require("util");
-
 let app = express();
 
 // Make sure vital env vars exist
@@ -26,20 +27,41 @@ app.use(morgan(
     process.env.NODE_ENV.indexOf("dev") != -1
     ? "dev" : "combined"
 ));
-
-// Serve static resources
-app.use(express.static(__dirname + "/static"));
+app.use(bodyParser.json());
 
 // Connect to postgres
 let urlConfig = pgcs.parse(process.env.DATABASE_URL);
 let dbConfig = util._extend(urlConfig, {
     max: 20,
-    idleTimeoutMillis: 10000
+    idleTimeoutMillis: 10000,
+    ssl: true
 });
 
-global.db = new pg.Pool(dbConfig);
+let db = new pg.Pool(dbConfig);
 
+// Serve static resources
+app.use(express.static(__dirname + "/static"));
+
+// Serve API
+app.post("/api/respond", require("./api/responder")(db));
+
+// Handle errors
+app.use(function(err, req, res, next){ // eslint-disable-line no-unused-vars
+    if(err instanceof assert.AssertionError){
+        res.status(400).json({
+            status:"err",
+            msg:"Request did not validate."
+        });
+    } else {
+        let errStatus = err.status || parseInt(err) || 500;
+        res.status(errStatus);
+        res.json({
+            status:"err",
+            msg: httpStatus.getStatusText(errStatus)
+        });
+    }
+});
 
 // Listen on PORT
-console.log("Listening on http://localhost:8080");
+console.log("Listening on http://localhost:"+process.env.PORT);
 app.listen(process.env.PORT);
