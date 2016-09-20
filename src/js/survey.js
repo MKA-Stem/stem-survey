b2d319932a802c572994d7e98b7797841913ca8d
William Goodall <wgoodall01@gmail.com>
2016-09-20 12:03:23 -0400

    Implement choice picking on survey

--- src/js/survey.js
+++ src/js/survey.js
@@ -1,33 +1,41 @@
 var app = angular.module("stem-survey", []);
 
 app.controller("formCtrl", function($scope, $http) {
-    $scope.loading = false;
-    $scope.error = false;
+    var vm = this;
+
+    vm.loading = false;
+    vm.error = false;
     
     var updatePlaceholder = function(){
-        if($scope.firstname && $scope.lastname){
-            $scope.emailPlaceholder =
-                `${$scope.firstname.substring(0, 1).toLowerCase()}${$scope.lastname.toLowerCase()}2020@mka.org`;
+        if(vm.firstname && vm.lastname){
+            vm.emailPlaceholder =
+                `${vm.firstname.substring(0, 1).toLowerCase()}${vm.lastname.toLowerCase()}2020@mka.org`;
         } else {
-            $scope.emailPlaceholder = "";
+            vm.emailPlaceholder = "";
         }
     };
 
-    $scope.$watch("firstname", updatePlaceholder);
-    $scope.$watch("lastname", updatePlaceholder);
+    $scope.$watch(function(){ return vm.firstname; }, updatePlaceholder);
+    $scope.$watch(function(){ return vm.lastname; }, updatePlaceholder);
 
 
-    $scope.submit = function(){
+    vm.submit = function(){
         var body = {
-            firstname: $scope.firstname,
-            lastname: $scope.lastname,
-            email: $scope.email || undefined
+            firstname: vm.firstname,
+            lastname: vm.lastname,
+            email: vm.email || undefined,
+            choice: vm.choice
         };
 
         $http.post("api/respond", body)
-        .then(function success(){$scope.loading = false;},
-              function error(){$scope.loading = false; $scope.error = true;});
+        .then(function success(){vm.loading = false;},
+              function error(){vm.loading = false; vm.error = true;});
 
-        $scope.loading = true;
+        vm.loading = true;
     };
+
+
+    $http.get("api/options")
+    .then(function success(res){console.log(res.data.options); vm.options = res.data.options; },
+          function fail(){alert("There was an error!"); });
 });
